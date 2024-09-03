37169bb8f4babf7781e3ab2d226968344ba5ae82
Maximiliano Duran <maximiliano.duran114@gmail.com>
2024-09-03 19:29:50 -0400

    empezando la animacion

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,14 +6,9 @@ import { HomePage } from './home.page';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HomePageRoutingModule } from './home-routing.module';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage],schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-import { createAnimation, Animation } from '@ionic/core';
+import {
+  Component,
+  ElementRef,
+  ViewChildren,
+  AfterViewInit,
+  QueryList,
+} from '@angular/core';
+import { AnimationController, Animation } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements AfterViewInit {
+  @ViewChildren('swiperSlide', { read: ElementRef }) slides!: QueryList<
+    ElementRef<HTMLDivElement>
+  >;
+
+  private animation!: Animation;
+
+  constructor(private animationCtrl: AnimationController) {}
+
+  ngAfterViewInit() {
+    this.slides.forEach((slide) => {
+      this.animation = this.animationCtrl
+        .create()
+        .addElement(slide.nativeElement)
+        .duration(1500)
+        .fromTo('transform', 'translateX(0px)', 'translateX(-100px)')
+        .fromTo('opacity', '1', '0.2');
+    });
+  }
+  play() {
+    this.animation.play();
+  }
 }
